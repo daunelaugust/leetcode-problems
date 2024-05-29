1e843e718eb3056bbf668dffb9020b405a1d9625
daunelaugust <daunelaugustin@gmail.com>
2024-05-29 18:56:32 -0400

    redid topKfreq using Neetcode bucket sort alg

--- python/topKFrequent.py
+++ python/topKFrequent.py
@@ -1,18 +1,23 @@
 def topKFrequent(self, nums: List[int], k: int) -> List[int]:
 
-        result =[]
-        number_map = {}
-        count = 1
+    #uses Neetcode bucket sort method which is bettern than my O(nlogn) solution this is O(n) time
 
-        for i in nums:
-            if i not in number_map:
-                number_map[i] = 1
-            else:
-                number_map[i] = number_map[i]+ 1
- 
-        for key, value in sorted(number_map.items(), key=lambda item: item[1], reverse=True):
-            if count > k:
-                break
-            else:
-                result.append(key)
-                count+=1
+    result =[[] for i in range(len(nums)+1)]
+    number_map = {}
+    final_result = []
+
+    for i in nums:
+        if i not in number_map:
+            number_map[i] = 1
+        else:
+            number_map[i] = number_map[i]+ 1
+
+    for key, value in number_map.items():
+        result[value].append(key)
+
+    for i in range(len(result) - 1, 0 , -1):
+        for val in result[i]:
+            final_result.append(val) 
+            if len(final_result) == k:
+                return final_result
+    
