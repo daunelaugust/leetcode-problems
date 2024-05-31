a7cd0735b8c9b305b59679cbeb05ac158558aaa5
daunelaugust <daunelaugustin@gmail.com>
2024-05-30 20:03:04 -0400

    optimized solution

--- python/productOfArrayExceptItself.py
+++ python/productOfArrayExceptItself.py
@@ -4,21 +4,15 @@ def productExceptSelf(self, nums: List[int]) -> List[int]:
     prefix = 1
     postfix = 1
     for i in range(len(nums)):
-        if i == 0:
-            res[i] = 1
-            continue
-        prefix = prefix * nums[i-1]
         res[i] = prefix
+        prefix = prefix * nums[i]
+        
 
     for i in range(len(nums)-1, -1, -1):
-        if i == len(nums)-1:
-            postfix = postfix * nums[i]
-            continue
         res[i] = postfix*res[i]
         postfix = postfix * nums[i]
     
-    
-
     return res
+
 
         
