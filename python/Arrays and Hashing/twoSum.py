f9dd605262f062a15fd00289a092dddbc2eea9bc
daunelaugust <daunelaugustin@gmail.com>
2024-07-27 19:00:48 -0400

    redid two sum

--- python/Arrays and Hashing/twoSum.py
+++ python/Arrays and Hashing/twoSum.py
@@ -1,17 +1,27 @@
 def twoSum(self, nums: List[int], target: int) -> List[int]:
-    #init map
-    maps={}
+    # #init map
+    # maps={}
 
-    #loop through the list
-    for i in range(len(nums)):
+    # #loop through the list
+    # for i in range(len(nums)):
 
-        #calculate the difference for each element
-        diff = target - nums[i]
+    #     #calculate the difference for each element
+    #     diff = target - nums[i]
 
-        #check if difference exist already in map
-        if diff in maps:
-            # if it exist return the index of the element where the correct diff was found and the current position 
-            return [maps[diff], i]
+    #     #check if difference exist already in map
+    #     if diff in maps:
+    #         # if it exist return the index of the element where the correct diff was found and the current position 
+    #         return [maps[diff], i]
         
-        #store current number as key  with index as value
+    #     #store current number as key  with index as value
+    #     maps[nums[i]] = i
+
+    #TWO SUM Redo 07/27/24
+        
+    maps = {}
+
+    for i in range(len(nums)):
+        if maps and target - nums[i] in maps:
+            return [maps[target - nums[i]], i]
+        
         maps[nums[i]] = i
