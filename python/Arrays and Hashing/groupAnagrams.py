190a7737605c109fe3c774e71c8ab37d477b7dec
daunelaugust <daunelaugustin@gmail.com>
2024-07-27 20:37:17 -0400

    redid both groupAnagrams

--- python/Arrays and Hashing/groupAnagrams.py
+++ python/Arrays and Hashing/groupAnagrams.py
@@ -1,18 +1,33 @@
 def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-    #Initialize a map
+#     #Initialize a map
 
-    word_map = {}
+#     word_map = {}
 
-    # for each word sort it and store as a key for dict
+#     # for each word sort it and store as a key for dict
+#     for i in strs:
+#         word = sorted(i)
+#         word = ''.join(word)
+#         # for the current word store as a value for the stored anagram 
+#         # The sorted key will be the same for all anagrams
+#         if word not in word_map:
+#             word_map[word] = []
+#         word_map[word].append(i)
+
+
+#     return word_map.values()
+
+
+# redo on 07/27/24
+
+    maps = defaultdict(list)
+
+    res = []
+
     for i in strs:
-        word = sorted(i)
-        word = ''.join(word)
-        # for the current word store as a value for the stored anagram 
-        # The sorted key will be the same for all anagrams
-        if word not in word_map:
-            word_map[word] = []
-        word_map[word].append(i)
+        array = 26 * [0]
+        for j in i:
+            array[ord(j)-97] += 1
+        maps[tuple(array)].append(i)
 
-
-    return word_map.values()
-
+    return list(maps.values())
+    
--- python/Arrays and Hashing/validAnagram.py
+++ python/Arrays and Hashing/validAnagram.py
@@ -1,24 +1,37 @@
 def isAnagram(self, s: str, t: str) -> bool:
 
-    # Make a map to store the freq of each char
-    str1 ={}
+    # # Make a map to store the freq of each char
+    # str1 ={}
 
-    #Loop thru first string
+    # #Loop thru first string
+    # for i in s:
+    #     #updates freqs of exisiting char
+    #     if i in str1:
+    #         str1[i] = str1[i] + 1
+    #     # inits a new char to 1
+    #     else:
+    #         str1[i] = 1
+
+    # #repeat for second string
+    # str2 ={}
+    # for i in t:
+    #     if i in str2:
+    #         str2[i] = str2[i] + 1
+    #     else:
+    #         str2[i] = 1
+
+    # # compares both string contents, using 'is' compares if its the same object instead
+    # return str1 == str2
+
+    #redo on 07/27/24
+    if len(s) != len(t):
+        return False
+    map1 = {}
+    map2 = {}
     for i in s:
-        #updates freqs of exisiting char
-        if i in str1:
-            str1[i] = str1[i] + 1
-        # inits a new char to 1
-        else:
-            str1[i] = 1
+        map1[i] = map1.get(i,0)+1
 
-    #repeat for second string
-    str2 ={}
     for i in t:
-        if i in str2:
-            str2[i] = str2[i] + 1
-        else:
-            str2[i] = 1
-
-    # compares both string contents, using 'is' compares if its the same object instead
-    return str1 == str2
+        map2[i] = map2.get(i,0)+1
+    
+    return map1 == map2
