e7a028d40d01a20d60f09d090e28ee586caf660c
daunelaugust <daunelaugustin@gmail.com>
2024-09-27 17:03:37 -0400

    optimal merge two  linked lists

--- python/LinkedList/MergeTwoSortedList.py
+++ python/LinkedList/MergeTwoSortedList.py
@@ -8,42 +8,65 @@ class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
 
     
-        res = []
+        # res = []
+
+        # while list1 and list2:
+
+        #     if  list1.val <= list2.val:
+        #         res.append(list1.val)
+        #         list1 = list1.next
+        #     else:
+        #         res.append(list2.val)
+        #         list2 = list2.next
+
+        # if list1:
+        #     while list1 != None:
+        #         res.append(list1.val)
+        #         list1 = list1.next
+        # elif list2:
+        #     while list2 != None:
+        #         res.append(list2.val)
+        #         list2 = list2.next
+
+
+        # if not res:
+        #     return None
+        
+        # # make head of list
+        # head = ListNode(res[0])
+        # current = head
+        
+        # # iterate through the values and create the rest of the nodes
+        # for value in res[1:]:
+        #     current.next = ListNode(value)
+        #     current = current.next
+        
+        
+        # return head
+        
+
+        #Optimal solution for merge two lists
+
+        dummy = ListNode()
+
+        head = dummy
 
         while list1 and list2:
 
-            if  list1.val <= list2.val:
-                res.append(list1.val)
+            if list1.val < list2.val:
+                dummy.next = list1
                 list1 = list1.next
             else:
-                res.append(list2.val)
+                dummy.next = list2
                 list2 = list2.next
-
+            dummy = dummy.next
+        
         if list1:
-            while list1 != None:
-                res.append(list1.val)
-                list1 = list1.next
-        elif list2:
-            while list2 != None:
-                res.append(list2.val)
-                list2 = list2.next
+            dummy.next = list1
+        else:
+            dummy.next = list2
 
-
-        if not res:
-            return None
-        
-        # make head of list
-        head = ListNode(res[0])
-        current = head
-        
-        # iterate through the values and create the rest of the nodes
-        for value in res[1:]:
-            current.next = ListNode(value)
-            current = current.next
-        
-        
-        return head
-        
+        return head.next
 
 
        
