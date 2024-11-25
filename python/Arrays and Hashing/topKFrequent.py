ccdc50b34e4236d7a2061dc71731048a6164b79f
daunelaugust <daunelaugustin@gmail.com>
2024-11-25 18:34:30 -0500

    redo two sum and top k freq

--- python/Arrays and Hashing/topKFrequent.py
+++ python/Arrays and Hashing/topKFrequent.py
@@ -1,6 +1,6 @@
 def topKFrequent(self, nums: List[int], k: int) -> List[int]:
 
-    #uses Neetcode bucket sort method which is bettern than my O(nlogn) solution this is O(n) time
+    # uses Neetcode bucket sort method which is bettern than my O(nlogn) solution this is O(n) time
 
     # result =[[] for i in range(len(nums)+1)]
     # number_map = {}
@@ -17,28 +17,48 @@ def topKFrequent(self, nums: List[int], k: int) -> List[int]:
 
     # for i in range(len(result) - 1, 0 , -1):
     #     for val in result[i]:
-    #         final_result.append(val) 
+    #         final_result.append(val)
     #         if len(final_result) == k:
     #             return final_result
-            
 
-#redo 07/28/24
-        maps = {}
+    # redo 07/28/24
+    maps = {}
+    for i in nums:
+        maps[i] = maps.get(i, 0) + 1
+    res = [[] for i in range(len(nums) + 1)]
+
+    for t, v in maps.items():
+        res[v].append(t)
+
+    output = []
+
+    for i in range(len(res) - 1, -1, -1):
+        for j in res[i]:
+            output.append(j)
+            if len(output) == k:
+                return output
+
+
+# redo 11/25/24 took a while to get
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+        resDict = {}
+
+        freqList = [[] for i in range(len(nums) + 1)]
+        res = []
+
         for i in nums:
-            maps[i] = maps.get(i,0)+1
-        res = [[] for i in range(len(nums)+1)]
+            if i in resDict:
+                resDict[i] += 1
+            else:
+                resDict[i] = 1
 
-        for t,v in maps.items():
-            res[v].append(t)
+        for t, v in resDict.items():
+            freqList[v].append(t)
 
-
-
-        output = []
-
-        for i in range(len(res)-1, -1, -1):
-            for j in res[i]:
-                output.append(j)
-                if len(output) == k:
-                    return output
-                
-    
+        for i in range(len(freqList) - 1, 0, -1):
+            for j in freqList[i]:
+                res.append(j)
+                if k == len(res):
+                    return res
--- python/Arrays and Hashing/twoSum.py
+++ python/Arrays and Hashing/twoSum.py
@@ -10,18 +10,28 @@ def twoSum(self, nums: List[int], target: int) -> List[int]:
 
     #     #check if difference exist already in map
     #     if diff in maps:
-    #         # if it exist return the index of the element where the correct diff was found and the current position 
+    #         # if it exist return the index of the element where the correct diff was found and the current position
     #         return [maps[diff], i]
-        
+
     #     #store current number as key  with index as value
     #     maps[nums[i]] = i
 
-    #TWO SUM Redo 07/27/24
-        
+    # TWO SUM Redo 07/27/24
+
     maps = {}
 
     for i in range(len(nums)):
         if maps and target - nums[i] in maps:
             return [maps[target - nums[i]], i]
-        
+
         maps[nums[i]] = i
+
+    # redo 11/25/24 had to run twice before submitting
+    diff = {}
+    for i in range(len(nums)):
+
+        if nums[i] in diff:
+            return [diff[nums[i]], i]
+        res = target - nums[i]
+
+        diff[res] = i
